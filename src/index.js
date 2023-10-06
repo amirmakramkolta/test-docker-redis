10a3399dae0ab2aa72fc5c8ba2fe793dad9d77d8
Amir Makram <35640764+amirmakramkolta@users.noreply.github.com>
2023-10-07 00:14:13 +0300

    change redis port and put try and catch

--- src/index.js
+++ src/index.js
@@ -9,33 +9,43 @@ const app = express()
 app.use(cors())
 
 const PORT = 3000
-const DEFAULT_EXPERITION = 3600
+const DEFAULT_EXPERITION = 6379
 
 app.get("/",(req, res)=>{
     res.send("Welcome from docker")
 })
 
 app.get("/photos",async (req, res)=>{
-    const albumId = req.query.albumId
+    try{
+        const albumId = req.query.albumId
+    
+        redisClient.get("photos",async (error, photos)=>{
+            if(error) console.error(error)
+            if(photos){
+                res.json(JSON.parse(photos));
+            }
+            else{
+                const {data} = await axios.get("https://jsonplaceholder.typicode.com/photos",{params:{albumId}})
+                redisClient.setEx("photos", DEFAULT_EXPERITION, JSON.stringify(data))
+                res.json(data)
+    
+            }
+        })
 
-    redisClient.get("photos",async (error, photos)=>{
-        if(error) console.error(error)
-        if(photos){
-            res.json(JSON.parse(photos));
-        }
-        else{
-            const {data} = await axios.get("https://jsonplaceholder.typicode.com/photos",{params:{albumId}})
-            redisClient.setEx("photos", DEFAULT_EXPERITION, JSON.stringify(data))
-            res.json(data)
-
-        }
-    })
+    }catch(error){
+        res.json(error)
+    }
 
 })
 app.get("/photos:id",async (req, res)=>{
-    const id = req.params.id
-    const {data} = await axios.get(`https://jsonplaceholder.typicode.com/photos/${id}`)
-    res.json(data)
+    try{
+        const id = req.params.id
+        const {data} = await axios.get(`https://jsonplaceholder.typicode.com/photos/${id}`)
+        res.json(data)
+    }
+    catch(error){
+        res.json(error)
+    }
 })
 
 app.listen(PORT, ()=>{
